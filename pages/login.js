8da3ed4658ae58c4db3b610ff69095f7bca072fe
JustNat <gabrielcp4@outlook.com>
2022-05-25 18:27:52 -0300

    :sparkles: feat: Adicionando componenetes internos de Login

---
+++ pages/login.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const AppContainer = props => <div>{props.children}</div>
+const AppInput = props => (
+    <div>
+        <label>{props.children}</label>
+        <input type={props.type} title={props.title}></input>
+    </div>
+)
+const AppButton = props => (
+    <div>
+        <input type="submit" title={props.tytle}></input>
+    </div>
+)
+
+const Login = props => {
+    return (
+      <AppContainer>
+        <AppInput 
+            title="apelido ou email do usuário"
+            label="Usuário"
+            type="text"
+        />
+        <AppInput 
+            label="Senha"
+            type="Password"
+            title="Senha do usuário"
+        />
+        <AppButton
+            title="Entrar"
+        />
+      </AppContainer>
+    );
+  };
+  
+  export default Login;
